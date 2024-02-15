dfca2d3b94f15da283bd4066f8668e58a6e39214
24-20 <106987713+24-20@users.noreply.github.com>
2024-02-15 19:00:35 +0100

    adding accordion to sidebaar, going to make tweeks so it fits design

--- frontend/src/componentContent/SidebarContent.tsx
+++ frontend/src/componentContent/SidebarContent.tsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import SidebarFooter from '@/components/ui/SidebarFooter'
-import SidebarElement from '@/components/ui/SidebarElement'
+import SidebarAccordionAlerts from '@/components/ui/SidebarAccordionAlerts'
 const SidebarContent = () => {
   return (
     <div className=' w-full h-screen  flex flex-col'>
         <div className=' px-[10px] pt-[30px] w-full h-screen  flex flex-col gap-[15px] '>
             <div className=' h-12 bg-red-600'>Logo</div>
             <h4>Hydrologiske varsler</h4>
-            <SidebarElement alertId={20} >item1</SidebarElement>
-            <SidebarElement alertId={20} >item2</SidebarElement>
-            <SidebarElement alertId={20} >item3</SidebarElement>
-            <SidebarElement alertId={20} >item4</SidebarElement>
+            <SidebarAccordionAlerts />
             <h4>varsel logg</h4>
-            <SidebarElement alertId={20} >item1</SidebarElement>
-            <SidebarElement alertId={20} >item2</SidebarElement>
-            <SidebarElement alertId={20} >item3</SidebarElement>
-            <SidebarElement alertId={20} >item4</SidebarElement>
+            <SidebarAccordionAlerts />
         </div>
         
         
