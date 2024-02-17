da2ae194a81969196c25b44ad5a904abf01cde67
24-20 <106987713+24-20@users.noreply.github.com>
2024-02-17 16:21:05 +0100

    adding venv and setting up backend server

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 }
 const Card = ({children, className=''}:CardProps) => {
   return (
-    <div className={cn(className,' shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] min-w-[300px] flex flex-grow h-[500px] bg-card ml-6 rounded-lg justify-center flex-row')}>
+    <div className={cn(className,' shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] min-w-[300px] flex flex-grow h-[500px] bg-card ml-6 rounded-lg justify-center flex-row')}>
         {
             children
         }
