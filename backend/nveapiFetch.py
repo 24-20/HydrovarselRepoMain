a1ce4547a96f436ce8a741105c27ef0d07ae71bc
24-20 <106987713+24-20@users.noreply.github.com>
2024-03-06 21:32:51 +0100

    backend changes

---
+++ backend/nveapiFetch.py
@@ -0,0 +1,27 @@
+from urllib.request import Request, urlopen
+import json
+async def nveapiFetch(reqdata):
+    #1000 = vannstand, 1001 = discharge, water tempratur = 1003, air tempratur = 17
+    print('------------------')
+    print(reqdata)
+    StationId = reqdata['StationId']
+    parameter = reqdata['parameter']
+    resolution_time = reqdata['resolution_time'] 
+    reference_time = reqdata['reference_time']
+    request_headers = {
+        "Accept": "application/json",
+        "X-API-Key": "RYgyp6MSs0+zuCqnuMaj4A=="
+    }
+
+    url = "https://hydapi.nve.no/api/v1/Observations?StationId=%s&Parameter=%s&ResolutionTime=%s&ReferenceTime=%s" %(StationId, parameter,resolution_time,reference_time,)
+    #url = "https://hydapi.nve.no/api/v1/Observations?StationId=212.27.0&Parameter=1001&ResolutionTime=0&ReferenceTime=P7D/"
+    request = Request(url, headers=request_headers)
+    response = urlopen(request)
+    content = response.read().decode('utf-8')
+
+    parsed_result = json.loads(content)
+
+
+    
+    return parsed_result
+    
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from get_all_stations import get_all_stations
+from nveapiFetch import nveapiFetch
 app = Flask(__name__)
 CORS(app)
 
@@ -9,6 +10,21 @@ async def get_contacts():
     print('hello there')
     all_stations = await get_all_stations()
     return {"data": all_stations}
+
+
+@app.route("/mesurments", methods=['POST'])
+#{stationName: 'Eibyelva v/Hammeren', stationId: '212.27.0', river: 'Eibyelva', kommune: 'Alta', kommuneNumber: '5403', …}
+#{parameter: '1001', resolution_time: 0, reference_time: 'P7D/'}
+async def getM():
+    reqdata = request.json
+    print(reqdata)
+    print('step 1')
+    apidata = await nveapiFetch(reqdata)
+    print('step 2')
+
+    print('finished')
+    #return apidata
+    return {'data':apidata}
 
 if __name__ == "__main__":
     app.run(debug=True)
